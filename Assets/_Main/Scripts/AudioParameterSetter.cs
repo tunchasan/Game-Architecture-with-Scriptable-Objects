db0a760252693ad569facfcf2ff06b8b8aa5669b
tunchasan <hasantunc.dev@gmail.com>
2021-03-31 05:54:45 +0300

    Complete AudioSystem

---
+++ Assets/_Main/Scripts/AudioParameterSetter.cs
@@ -0,0 +1,30 @@
+﻿using Project.Architecture;
+using Project.Architecture.ScriptableObjects.Variables;
+using UnityEngine;
+using UnityEngine.Audio;
+
+namespace Project
+{
+    public class AudioParameterSetter : MonoBehaviour
+    {
+        [Tooltip("Mixer to set the parameter in.")]
+        [SerializeField] private AudioMixer mixer;
+
+        [SerializeField] private string parameterName = "";
+
+        [Tooltip("Variable to send to the mixer parameter.")]
+        [SerializeField] private FloatVariable variable;
+
+        [Tooltip("Curve to evaluate in order to look up a final value to send as the parameter.\n" +
+                 "T=0 is when Variable == Min\n" +
+                 "T=1 is when Variable == Max")]
+        [SerializeField] private AnimationCurve curve;
+
+        private void Update()
+        {
+            var t = Mathf.InverseLerp(0, 20, variable);
+            var value = curve.Evaluate(Mathf.Clamp01(t));
+            mixer.SetFloat(parameterName, value);
+        }
+    }
+}
---
+++ Assets/_Main/Scripts/VariableAudioTrigger.cs
@@ -0,0 +1,26 @@
+﻿using Project.Architecture.ScriptableObjects.Variables;
+using UnityEngine;
+
+namespace Project
+{
+    public class VariableAudioTrigger : MonoBehaviour
+    {
+        [SerializeField] private AudioSource audioSource;
+
+        [SerializeField] private FloatVariable variable;
+
+        private void Update()
+        {
+            if (variable < 20)
+            {
+                if (!audioSource.isPlaying)
+                    audioSource.Play();
+            }
+            else
+            {
+                if (audioSource.isPlaying)
+                    audioSource.Stop();
+            }
+        }
+    }
+}
